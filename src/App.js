6cb6f7456699af4bc2e86dd428336b3c9d852748
Cindy Wong <cindywongdev@gmail.com>
2023-01-06 16:27:08 -0500

    created Nav component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import './App.css';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
+      <Nav/>
       {/* Outlet component connects the app to the router */}
       <Outlet/>
     </div>
