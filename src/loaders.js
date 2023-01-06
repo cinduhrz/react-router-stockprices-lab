555c30f8a1abc8774b48211bac1f871a80a0dc30
Cindy Wong <cindywongdev@gmail.com>
2023-01-06 17:35:34 -0500

    created priceLoader loader function

---
+++ src/loaders.js
@@ -0,0 +1,5 @@
+export const priceLoader = async ({params}) => {
+    const symbol = params.symbol
+    const apiKey = "70c41fcde63658424146c7d27206b597"
+    return (await fetch(`https://financialmodelingprep.com/api/v3/quote-short/${symbol}?apikey=${apiKey}`)).json()
+}
